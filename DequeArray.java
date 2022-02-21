d90ae118a6950269c4f076257d22c25398a193c5
ameeninfobhan <developer@infobhan.cloud>
2022-02-21 11:06:33 +0530

    Add Last Element in Dequeue using Array

--- DequeArray.java
+++ DequeArray.java
@@ -30,20 +30,42 @@ public class DequeArray implements Iterable<Item>  {
         return n;
     }
 
+    // resize the underlying array
+    private void resize(int capacity) {
+        assert capacity >= n;
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
+            copy[i] = q[(first + i) % q.length];
+        }
+        q = copy;
+        first = 0;
+        last  = n;
+    }
+
     // public void addFirst(Item item)
+    public void addLast(Item item)
+    {
+        if (n == q.length) resize(2*q.length);   // double size of array if necessary
+        q[last++] = item;                        // add item
+        if (last == q.length) last = 0;          // wrap-around
+        n++;
+    }
 
-    // public void addLast(Item item)
-
-    // public Item removeFirst()
+    //public Item removeFirst()
+    
 
     // public Item removeLast()
+
+    public Iterator<Item> iterator() {
+        return new DequeArray();
+    }
 
     public static void main(String[] args)
     {
         try{
             File file = new File("C:\\GitFolder\\ADS\\M2\\QUEUES\\test1.txt");
         Scanner sc= new Scanner(file);
-        Deque<String> dq= new Deque<>();
+        Deque<String> dq= new Deque<String>();
 
         }catch(FileNotFoundException e){
             System.out.println("An error occured while reading file.");
